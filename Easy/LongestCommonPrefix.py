2b96709688d7970eda44922d96c31f966489aa95
Ammar Khan <ammar.khaan05@gmail.com>
2026-02-17 23:57:49 +0500

    Added: Easy - Longest Common Prefix

---
+++ Easy/LongestCommonPrefix.py
@@ -0,0 +1,19 @@
+strs = ["flower", "flow", "flight"]
+
+
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+
+    prefix = strs[0]
+
+    for i in range(1, len(strs)):
+        while strs[i].find(prefix) != 0:
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+
+    return prefix
+
+
+print(longestCommonPrefix(strs))
